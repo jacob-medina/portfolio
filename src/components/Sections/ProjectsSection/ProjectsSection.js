659c058b993e8ff6583bf8e3a5d4f4ec21f65a09
Jacob Medina <jacobmedina1618@gmail.com>
2023-11-01 17:15:55 -0400

    added uFree project

--- src/components/Sections/ProjectsSection/ProjectsSection.js
+++ src/components/Sections/ProjectsSection/ProjectsSection.js
@@ -6,6 +6,15 @@ import Project from "../../Project/Project";
 const projects = [
     {
         id: 1,
+        projectName: "uFree",
+        description: "A website to make event scheduling with a group seamless.",
+        previewImg: "ufree",
+        deployLink: "https://ufree-f24a66c626ea.herokuapp.com/",
+        repoLink: "https://github.com/hafsah1976/uFree",
+        toolsUsed: ['React', 'MongoDB', 'Express', 'Node', 'JavaScript', 'CSS'],
+    },
+    {
+        id: 2,
         projectName: "Rateit",
         description: "A website where users can find and rate programming tools based on different characteristics.",
         previewImg: "rateit",
@@ -14,7 +23,7 @@ const projects = [
         toolsUsed: ['Handlebars', 'Sequelize', 'Express', 'Node', 'Bootstrap', 'JavaScript', 'CSS'],
     },
     {
-        id: 2,
+        id: 3,
         projectName: "TotK Bingo",
         description: "A bingo board of randomly generated challenges for Tears of the Kingdom. Players can create virtual rooms and race each other in real time.",
         previewImg: "totkBingo",
@@ -23,7 +32,7 @@ const projects = [
         toolsUsed: ['Socket.io', 'Express', 'Node', 'jQuery', 'Bootstrap', 'JavaScript', 'HTML', 'CSS'],
     },
     {
-        id: 3,
+        id: 4,
         projectName: "Stackology",
         description: "A community run blog for full stack developers. Users can sign in to write blog posts and create comments.",
         previewImg: "stackology",
@@ -32,7 +41,7 @@ const projects = [
         toolsUsed: ['Handlebars', 'Sequelize', 'Express', 'Node', 'Bootstrap', 'JavaScript'],
     },
     {
-        id: 4,
+        id: 5,
         projectName: "Pixel Bee",
         description: "A community pixel art drawing board where each drawing fills a cell in the beehive.",
         previewImg: "pixelBee",
@@ -40,15 +49,15 @@ const projects = [
         repoLink: "https://github.com/jacob-medina/pixel-bee",
         toolsUsed: ['Express', 'Node', 'JavaScript', 'HTML', 'CSS'],
     },
-    {
-        id: 5,
-        projectName: "Odd Quote Odd: Kanye Edition",
-        description: "A quiz game where players spot quotes from Kanye West hidden among a pool of other quotes. Players can save quotes they see along the way for future review.",
-        previewImg: "oddQuoteOut",
-        deployLink: "https://iab-19.github.io/odd_quote_out_kanye_edition",
-        repoLink: "https://github.com/iab-19/odd_quote_out_kanye_edition",
-        toolsUsed: ['jQuery', 'Materialize', 'JavaScript', 'HTML', 'CSS'],
-    },
+    // {
+    //     id: 5,
+    //     projectName: "Odd Quote Odd: Kanye Edition",
+    //     description: "A quiz game where players spot quotes from Kanye West hidden among a pool of other quotes. Players can save quotes they see along the way for future review.",
+    //     previewImg: "oddQuoteOut",
+    //     deployLink: "https://iab-19.github.io/odd_quote_out_kanye_edition",
+    //     repoLink: "https://github.com/iab-19/odd_quote_out_kanye_edition",
+    //     toolsUsed: ['jQuery', 'Materialize', 'JavaScript', 'HTML', 'CSS'],
+    // },
 ];
 
 export default function ProjectsSection() {
--- src/images/project-previews/index.js
+++ src/images/project-previews/index.js
@@ -3,13 +3,15 @@ import pixelBee from './pixel-bee-preview.png';
 import rateit from './rateit-preview.png';
 import stackology from './stackology-preview.png';
 import totkBingo from './totk-bingo-preview.png';
+import ufree from './ufree-preview.png';
 
 const projectPreviews = {
     oddQuoteOut,
     pixelBee,
     rateit,
     stackology,
-    totkBingo
+    totkBingo,
+    ufree,
 }
 
 export default projectPreviews;
